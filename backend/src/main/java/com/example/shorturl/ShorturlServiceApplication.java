40e33ddd17dbf67d9f379266ba57f26e3c6a8d1b
Prathik R Gowda <137380806+prathikrgowda@users.noreply.github.com>
2025-07-18 20:02:48 -0400

    frontend and backend initialized

---
+++ backend/src/main/java/com/example/shorturl/ShorturlServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.shorturl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShorturlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShorturlServiceApplication.class, args);
+	}
+
+}
